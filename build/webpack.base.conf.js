5f21bac741269e9c31a6f6a9c2df2521713bbb0b
vedees <tichoo@yandex.ru>
2020-01-05 20:18:44 +0800

    remove vue. tamplete is clean now

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -6,7 +6,6 @@ const fs = require("fs");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { VueLoaderPlugin } = require("vue-loader");
 
 // Main const. Feel free to change it
 const PATHS = {
@@ -54,16 +53,6 @@ module.exports = {
         test: /\.js$/,
         loader: "babel-loader",
         exclude: "/node_modules/"
-      },
-      {
-        // Vue
-        test: /\.vue$/,
-        loader: "vue-loader",
-        options: {
-          loader: {
-            scss: "vue-style-loader!css-loader!sass-loader"
-          }
-        }
       },
       {
         // Fonts
@@ -127,12 +116,10 @@ module.exports = {
   },
   resolve: {
     alias: {
-      "~": PATHS.src,
-      vue$: "vue/dist/vue.js"
+      "~": PATHS.src
     }
   },
   plugins: [
-    new VueLoaderPlugin(),
     new MiniCssExtractPlugin({
       filename: `${PATHS.assets}css/[name].[contenthash].css`
     }),
--- src/index.js
+++ src/index.js
@@ -6,14 +6,3 @@ import './assets/scss/main.scss'
 
 // CSS (example)
 // import './assets/css/main.css'
-
-// Vue.js
-window.Vue = require('vue')
-
-// Vue components (for use in html)
-Vue.component('example-component', require('./components/Example.vue').default)
-
-// Vue init
-const app = new Vue({
-  el: '#app'
-})
